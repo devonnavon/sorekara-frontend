4a909d42efdd5794c6e139193d52a6661d57e76f
Devon Navon <devon.navon@gmail.com>
2020-08-05 16:26:04 -0700

    create event api thing done

--- src/utils/api/event.js
+++ src/utils/api/event.js
@@ -17,11 +17,34 @@ const events = async () => {
 	return await request(query, {}, headers);
 };
 
-const create = async () => {
+const create = async (eventFields) => {
 	const query = `
-    
+        mutation(
+            $title: String!
+            $description: String
+            $eventDate: DateTime
+            $published: Boolean
+            $password: String
+            ) {
+            createEvent(
+                title: $title
+                description: $description
+                eventDate: $eventDate
+                published: $published
+                password: $password
+            ) {
+                id
+                title
+                description
+                eventDate
+                published
+                password
+            }
+        }
+
     `;
-	// const variables =
+	const variables = eventFields;
+	return await request(query, variables, header);
 };
 
-export default { events };
+export default { events, create };
