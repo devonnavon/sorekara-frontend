941cef2b5bc84502f659de82c448e0baae4f89ec
Devon Navon <devon.navon@gmail.com>
2020-08-06 18:23:06 -0700

    update and create working, need to add form error checking

--- src/components/event/TheEventModal.vue
+++ src/components/event/TheEventModal.vue
@@ -1,175 +1,149 @@
 <template>
-	<div>
-		<BaseModal>
-			<!-- <TheLoginForm></TheLoginForm>
-      <TheRegisterForm></TheRegisterForm>-->
-			<transition name="fade" mode="out-in">
-				<form class>
-					<div class="text-center relative pb-5">
-						<div class="flex items-center justify-center"></div>
-						<h2 v-show="modalState === 'create'" class="text-4xl">
-							Create Event
-						</h2>
-						<h2 v-show="modalState === 'update'" class="text-4xl">
-							Update Event
-						</h2>
-					</div>
-					<div class="flex flex-wrap -mx-3 mb-6">
-						<div class="w-full md:w-full px-3 mb-6 text-opacity-60">
-							<label
-								class="block uppercase text-gray-700 text-xs font-bold mb-2"
-								for="Title"
-								>TITLE</label
-							>
-							<input
-								v-model="title"
-								class="appearance-none block w-full bg-white text-gray-900 border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none placeholder-orange"
-								type="text"
-								placeholder="A nice title"
-								required
-							/>
-						</div>
-						<div class="w-full md:w-full px-3 mb-6">
-							<label
-								class="block uppercase text-xs font-bold mb-2 text-opacity-60"
-								for="Description"
-								>DESCRIPTION</label
-							>
-							<textarea
-								v-model="description"
-								class="appearance-none block w-full bg-white border border-gray-400 rounded-lg py-3 px-3 focus:outline-none resize-none overflow-hidden placeholder-orange"
-								placeholder="A nice description, you don't have to if you don't want to though"
-							/>
-						</div>
-						<div class="w-full md:w-full px-3 mb-6">
-							<label
-								class="block uppercase text-xs font-bold mb-2 text-opacity-60"
-								for="eventDate"
-								>EVENT DATE</label
-							>
-							<input
-								v-model="eventDate"
-								class="block ml-0 pl-0 outline-current placeholder-orange focus:border-orange"
-								type="date"
-								name="eventDate"
-							/>
-						</div>
+  <div>
+    <BaseModal>
+      <transition name="fade" mode="out-in">
+        <form class>
+          <div class="text-center relative pb-5">
+            <div class="flex items-center justify-center"></div>
+            <h2 v-show="modalState === 'create'" class="text-4xl">Create Event</h2>
+            <h2 v-show="modalState === 'update'" class="text-4xl">Update Event</h2>
+          </div>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-full px-3 mb-6 text-opacity-60">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="Title">TITLE</label>
+              <input
+                v-model="title"
+                class="appearance-none block w-full bg-white text-gray-900 border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none placeholder-orange"
+                type="text"
+                placeholder="A nice title"
+                required
+              />
+            </div>
+            <div class="w-full md:w-full px-3 mb-6">
+              <label
+                class="block uppercase text-xs font-bold mb-2 text-opacity-60"
+                for="Description"
+              >DESCRIPTION</label>
+              <textarea
+                v-model="description"
+                class="appearance-none block w-full bg-white border border-gray-400 rounded-lg py-3 px-3 focus:outline-none resize-none overflow-hidden placeholder-orange"
+                placeholder="A nice description, you don't have to if you don't want to though"
+              />
+            </div>
+            <div class="w-full md:w-full px-3 mb-6">
+              <label
+                class="block uppercase text-xs font-bold mb-2 text-opacity-60"
+                for="eventDate"
+              >EVENT DATE</label>
+              <input
+                v-model="eventDate"
+                class="block ml-0 pl-0 outline-current placeholder-orange focus:border-orange"
+                type="date"
+                name="eventDate"
+              />
+            </div>
 
-						<div class="w-full md:w-full px-3 mb-6">
-							<label
-								class="block uppercase te text-xs mb-2 text-opacity-60"
-								for="Password"
-								>Password</label
-							>
-							<input
-								v-model="password"
-								class="appearance-none block w-full bg-white font-medium border border-gray-400 rounded-lg py-3 px-3 focus:outline-none placeholder-orange"
-								type="text"
-								placeholder="Leave blank for an open event!"
-							/>
-						</div>
-						<div
-							v-show="modalState === 'create'"
-							class="w-full md:w-full px-3 mb-6"
-						>
-							<button
-								@click="create"
-								@submit.prevent
-								class="appearance-none block w-full border border-gray-200 rounded-lg py-3 px-3 transition ease-in duration-200 hover:bg-orange hover:text-white focus:outline-none focus:bg-orange focus:text-white"
-							>
-								CREATE
-							</button>
-						</div>
-						<div
-							v-show="modalState === 'update'"
-							class="w-full md:w-full px-3 mb-6"
-						>
-							<button
-								@click="update"
-								@submit.prevent
-								class="appearance-none block w-full border border-gray-200 rounded-lg py-3 px-3 transition ease-in duration-200 hover:bg-orange hover:text-white focus:outline-none focus:bg-orange focus:text-white"
-							>
-								UPDATE
-							</button>
-						</div>
-					</div>
-				</form>
-			</transition>
-			<!-- <component v-bind:is="currentFormComponent"></component> -->
-		</BaseModal>
-	</div>
+            <div class="w-full md:w-full px-3 mb-6">
+              <label class="block uppercase te text-xs mb-2 text-opacity-60" for="Password">Password</label>
+              <input
+                v-model="password"
+                class="appearance-none block w-full bg-white font-medium border border-gray-400 rounded-lg py-3 px-3 focus:outline-none placeholder-orange"
+                type="text"
+                placeholder="Leave blank for an open event!"
+              />
+            </div>
+            <div v-show="modalState === 'create'" class="w-full md:w-full px-3 mb-6">
+              <button
+                @click.prevent="create"
+                class="appearance-none block w-full border border-gray-200 rounded-lg py-3 px-3 transition ease-in duration-200 hover:bg-orange hover:text-white focus:outline-none focus:bg-orange focus:text-white"
+              >CREATE</button>
+            </div>
+            <div v-show="modalState === 'update'" class="w-full md:w-full px-3 mb-6">
+              <button
+                @click.prevent="update"
+                class="appearance-none block w-full border border-gray-200 rounded-lg py-3 px-3 transition ease-in duration-200 hover:bg-orange hover:text-white focus:outline-none focus:bg-orange focus:text-white"
+              >UPDATE</button>
+            </div>
+          </div>
+        </form>
+      </transition>
+    </BaseModal>
+  </div>
 </template>
 <script>
-import BaseModal from './BaseModal.vue';
-import bus from '../../bus';
+import BaseModal from "./BaseModal.vue";
+import bus from "../../bus";
 
-import moment from 'moment';
+import moment from "moment";
 
 export default {
-	name: 'TheEventModal',
-	components: { BaseModal },
-	props: { event: Object },
-	data() {
-		return {
-			title: '',
-			description: '',
-			password: '',
-			eventDate: moment().format('YYYY-MM-DD'),
-		};
-	},
-	watch: {
-		event(event) {
-			if (event) {
-				this.title = event.title;
-				this.description = event.description;
-				this.password = event.password;
-				this.eventDate = moment(event.eventDate).format('YYYY-MM-DD');
-			} else {
-				this.title = '';
-				this.description = '';
-				this.password = '';
-				this.eventDate = moment().format('YYYY-MM-DD');
-			}
-		},
-	},
-	computed: {
-		modalState() {
-			if (this.event) {
-				return 'update';
-			} else {
-				return 'create';
-			}
-		},
-	},
-	methods: {
-		async create() {
-			const newEvent = await this.$api.event.create({
-				title: this.title,
-				description: this.description,
-				password: this.password,
-				eventDate: moment(this.eventDate),
-			});
-			console.log(newEvent);
-		},
-		async update() {
-			const updatedEvent = await this.$api.event.update({
-				id: this.event.id,
-				title: this.title,
-				description: this.description,
-				password: this.password,
-				eventDate: moment(this.eventDate),
-			});
-		},
-	},
+  name: "TheEventModal",
+  components: { BaseModal },
+  props: { event: Object },
+  data() {
+    return {
+      title: "",
+      description: "",
+      password: "",
+      eventDate: moment().format("YYYY-MM-DD"),
+    };
+  },
+  watch: {
+    event(event) {
+      if (event) {
+        this.title = event.title;
+        this.description = event.description;
+        this.password = event.password;
+        this.eventDate = moment(event.eventDate).format("YYYY-MM-DD");
+      } else {
+        this.title = "";
+        this.description = "";
+        this.password = "";
+        this.eventDate = moment().format("YYYY-MM-DD");
+      }
+    },
+  },
+  computed: {
+    modalState() {
+      if (this.event) {
+        return "update";
+      } else {
+        return "create";
+      }
+    },
+  },
+  methods: {
+    async create() {
+      const newEvent = await this.$api.event.create({
+        title: this.title,
+        description: this.description,
+        password: this.password,
+        eventDate: moment(this.eventDate),
+      });
+      bus.$emit("modal-close");
+      bus.$emit("event-created", newEvent);
+    },
+    async update() {
+      const updatedEvent = await this.$api.event.update({
+        id: this.event.id,
+        title: this.title,
+        description: this.description,
+        password: this.password,
+        eventDate: moment(this.eventDate),
+      });
+      bus.$emit("modal-close");
+      bus.$emit("event-updated", updatedEvent);
+    },
+  },
 };
 </script>
 <style>
 .fade-enter-active,
 .fade-leave-active {
-	transition: opacity 0.5s !important;
+  transition: opacity 0.5s !important;
 }
 .fade-enter,
 .fade-leave-to {
-	opacity: 0 !important;
+  opacity: 0 !important;
 }
 </style>
--- src/components/layout/TheEventTable.vue
+++ src/components/layout/TheEventTable.vue
@@ -1,187 +1,178 @@
 <template>
-	<div>
-		<TheEventModal :event="selectedEvent" v-show="isModalVisible" />
+  <div>
+    <TheEventModal :event="selectedEvent" v-show="isModalVisible" />
 
-		<div class="pt-10">
-			<div class="flex flex-row justify-between pb-5">
-				<div class="font-display text-orange text-3xl flex-initial">
-					My Events
-				</div>
-				<button
-					class="font-display text-orange text-4xl flex-initial focus:outline-none outline-none"
-				>
-					<IconifyIcon
-						:icon="icons.plusCircleOutlined"
-						class="text-orange inline fill-current transform hover:rotate-180 transition-transform duration-1000 ease-out self-center outline-none focus:outline-none"
-						@click="showModal()"
-					/>
-				</button>
-			</div>
-			<table class="border-collapse w-full font-display">
-				<thead>
-					<tr>
-						<th
-							class="text-base text-orange text-left text-opacity-60 hover:text-opacity-100 hidden lg:table-cell"
-						>
-							TITLE
-						</th>
-						<th
-							class="text-base text-orange text-left text-opacity-60 hover:text-opacity-100 hidden lg:table-cell"
-						>
-							DESCRIPTION
-						</th>
-						<th
-							class="text-base text-orange text-left text-opacity-60 hover:text-opacity-100 hidden lg:table-cell"
-						>
-							EVENT DATE
-						</th>
-						<th
-							class="text-base text-orange text-left text-opacity-60 hover:text-opacity-100 hidden lg:table-cell"
-						>
-							PASSWORD
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr
-						v-for="(event, id) in events"
-						:key="id"
-						class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0 border border-b-1 border-l-0 border-r-0 border-t-0 border-orange"
-					>
-						<td
-							class="w-full py-6 text-xl lg:w-1/6 lg:p-6 lg:pl-0 border border-b-0 text-center block border-opacity-60 font-display lg:text-base text-orange font-normal lg:align-top lg:table-cell relative lg:static lg:mb-0 lg:text-left lg:border-r-0 lg:border-l-0 lg:border-t-1 lg:border-orange lg:border-b-1"
-						>
-							<span
-								class="lg:hidden absolute top-0 left-0 px-2 py-1 text-xs uppercase block"
-								>TITLE</span
-							>
-							{{ event.title }}
-							<!-- event settings desktop -->
-							<IconifyIcon
-								@click="showModal(event)"
-								:icon="icons.settingOutlined"
-								class="visible lg:invisible text-orange fill-current h-6 w-6 absolute top-0 right-0 m-1"
-							/>
-						</td>
-						<td
-							class="w-full lg:w-2/5 py-6 px-2 lg:px-0 lg:p-6 lg:pl-0 border border-b-0 text-center block border-opacity-60 font-display text-base text-orange lg:align-top lg:table-cell relative lg:static lg:mb-0 lg:text-left lg:border-r-0 lg:border-l-0 lg:border-t-1 lg:border-orange lg:border-b-1 lg:pr-4"
-						>
-							<span
-								class="lg:hidden absolute top-0 left-0 px-2 py-1 text-xs uppercase block"
-								>DESCRIPTION</span
-							>
-							{{ event.description }}
-						</td>
-						<td
-							class="w-full lg:w-1/6 lg:p-3 lg:pl-0 border border-b-0 text-center block border-opacity-60 font-display text-base text-orange lg:align-top lg:table-cell relative lg:static lg:mb-0 lg:text-left lg:border-r-0 lg:border-l-0 lg:border-t-1 lg:border-b-1 lg:border-orange py-6 px-2 lg:pt-6"
-						>
-							<span
-								class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs uppercase"
-								>EVENT DATE</span
-							>
-							<span class="py-1 px-3 pl-0 text-base text-orange">
-								{{ formattedDate(event.eventDate) }}
-							</span>
-						</td>
-						<td
-							class="w-full lg:w-auto p-3 lg:pl-0 lg:pr-0 border border-b-0 text-center block border-opacity-60 font-display text-base text-orange lg:align-top lg:table-cell relative lg:static lg:mb-0 lg:text-left lg:border-r-0 lg:border-l-0 lg:border-t-1 lg:border-orange lg:relative lg:pt-6 lg:pb-1 pt-6 pb-2"
-						>
-							<!-- event settings mobile -->
-							<IconifyIcon
-								@click="showModal(event)"
-								:icon="icons.settingOutlined"
-								class="invisible lg:visible text-orange fill-current h-6 w-6 absolute top-0 right-0 p-1 transform rotate-45 hover:rotate-180 transition-transform duration-1000 ease-out"
-							/>
-							<span
-								class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs uppercase"
-								>PASSWORD</span
-							>
+    <div class="pt-10">
+      <div class="flex flex-row justify-between pb-5">
+        <div class="font-display text-orange text-3xl flex-initial">My Events</div>
+        <button
+          class="font-display text-orange text-4xl flex-initial focus:outline-none outline-none"
+        >
+          <IconifyIcon
+            :icon="icons.plusCircleOutlined"
+            class="text-orange inline fill-current transform hover:rotate-180 transition-transform duration-1000 ease-out self-center outline-none focus:outline-none"
+            @click="showModal()"
+          />
+        </button>
+      </div>
+      <table class="border-collapse w-full font-display">
+        <thead>
+          <tr>
+            <th
+              class="text-base text-orange text-left text-opacity-60 hover:text-opacity-100 hidden lg:table-cell"
+            >TITLE</th>
+            <th
+              class="text-base text-orange text-left text-opacity-60 hover:text-opacity-100 hidden lg:table-cell"
+            >DESCRIPTION</th>
+            <th
+              class="text-base text-orange text-left text-opacity-60 hover:text-opacity-100 hidden lg:table-cell"
+            >EVENT DATE</th>
+            <th
+              class="text-base text-orange text-left text-opacity-60 hover:text-opacity-100 hidden lg:table-cell"
+            >PASSWORD</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(event, id) in events"
+            :key="id"
+            class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0 border border-b-1 border-l-0 border-r-0 border-t-0 border-orange"
+          >
+            <td
+              class="w-full py-6 text-xl lg:w-1/6 lg:p-6 lg:pl-0 border border-b-0 text-center block border-opacity-60 font-display lg:text-base text-orange font-normal lg:align-top lg:table-cell relative lg:static lg:mb-0 lg:text-left lg:border-r-0 lg:border-l-0 lg:border-t-1 lg:border-orange lg:border-b-1"
+            >
+              <span class="lg:hidden absolute top-0 left-0 px-2 py-1 text-xs uppercase block">TITLE</span>
+              {{ event.title }}
+              <!-- event settings desktop -->
+              <IconifyIcon
+                @click="showModal(event)"
+                :icon="icons.settingOutlined"
+                class="visible lg:invisible text-orange fill-current h-6 w-6 absolute top-0 right-0 m-1"
+              />
+            </td>
+            <td
+              class="w-full lg:w-2/5 py-6 px-2 lg:px-0 lg:p-6 lg:pl-0 border border-b-0 text-center block border-opacity-60 font-display text-base text-orange lg:align-top lg:table-cell relative lg:static lg:mb-0 lg:text-left lg:border-r-0 lg:border-l-0 lg:border-t-1 lg:border-orange lg:border-b-1 lg:pr-4"
+            >
+              <span
+                class="lg:hidden absolute top-0 left-0 px-2 py-1 text-xs uppercase block"
+              >DESCRIPTION</span>
+              {{ event.description }}
+            </td>
+            <td
+              class="w-full lg:w-1/6 lg:p-3 lg:pl-0 border border-b-0 text-center block border-opacity-60 font-display text-base text-orange lg:align-top lg:table-cell relative lg:static lg:mb-0 lg:text-left lg:border-r-0 lg:border-l-0 lg:border-t-1 lg:border-b-1 lg:border-orange py-6 px-2 lg:pt-6"
+            >
+              <span
+                class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs uppercase"
+              >EVENT DATE</span>
+              <span
+                class="py-1 px-3 pl-0 text-base text-orange"
+              >{{ formattedDate(event.eventDate) }}</span>
+            </td>
+            <td
+              class="w-full lg:w-auto p-3 lg:pl-0 lg:pr-0 border border-b-0 text-center block border-opacity-60 font-display text-base text-orange lg:align-top lg:table-cell relative lg:static lg:mb-0 lg:text-left lg:border-r-0 lg:border-l-0 lg:border-t-1 lg:border-orange lg:relative lg:pt-6 lg:pb-1 pt-6 pb-2"
+            >
+              <!-- event settings mobile -->
+              <IconifyIcon
+                @click="showModal(event)"
+                :icon="icons.settingOutlined"
+                class="invisible lg:visible text-orange fill-current h-6 w-6 absolute top-0 right-0 p-1 transform rotate-45 hover:rotate-180 transition-transform duration-1000 ease-out"
+              />
+              <span
+                class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs uppercase"
+              >PASSWORD</span>
 
-							<span class="py-1 px-3 pl-0 text-base inline">
-								{{ event.password }}
-								<!-- <button>
+              <span class="py-1 px-3 pl-0 text-base inline">
+                {{ event.password }}
+                <!-- <button>
 							<IconifyIcon
 								:icon="icons.eyeOutlined"
 								class="text-orange fill-current h-6 w-6 inline"
 							/>
                 </button>-->
-							</span>
-							<div class="flex justify-end align-bottom pt-5 lg:pt-8">
-								<button
-									class="appearance-none block w-auto border border-dotted py-1 px-2 mr-5 leading-tight transition ease-in duration-200 hover:bg-orange hover:text-white focus:bg-orange focus:text-white focus:bg-orange focus:text-white lg:mb-3"
-								>
-									PREVIEW
-								</button>
-								<button
-									class="appearance-none block w-auto border border-dotted py-1 px-2 leading-tight transition ease-in duration-200 hover:bg-orange hover:text-white focus:bg-orange focus:text-white focus:bg-orange focus:text-white lg:mb-3"
-								>
-									EDITOR
-								</button>
-							</div>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
+              </span>
+              <div class="flex justify-end align-bottom pt-5 lg:pt-8">
+                <button
+                  class="appearance-none block w-auto border border-dotted py-1 px-2 mr-5 leading-tight transition ease-in duration-200 hover:bg-orange hover:text-white focus:bg-orange focus:text-white focus:bg-orange focus:text-white lg:mb-3"
+                >PREVIEW</button>
+                <button
+                  class="appearance-none block w-auto border border-dotted py-1 px-2 leading-tight transition ease-in duration-200 hover:bg-orange hover:text-white focus:bg-orange focus:text-white focus:bg-orange focus:text-white lg:mb-3"
+                >EDITOR</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import TheEventModal from '../event/TheEventModal.vue';
-import bus from '../../bus';
-import moment from 'moment';
+import TheEventModal from "../event/TheEventModal.vue";
+import bus from "../../bus";
+import moment from "moment";
 
-import IconifyIcon from '@iconify/vue';
-import eyeOutlined from '@iconify/icons-ant-design/eye-outlined';
-import eyeInvisibleOutlined from '@iconify/icons-ant-design/eye-invisible-outlined';
-import settingOutlined from '@iconify/icons-ant-design/setting-outlined';
-import plusCircleOutlined from '@iconify/icons-ant-design/plus-circle-outlined';
-import plusCircleFilled from '@iconify/icons-ant-design/plus-circle-filled';
+import IconifyIcon from "@iconify/vue";
+import eyeOutlined from "@iconify/icons-ant-design/eye-outlined";
+import eyeInvisibleOutlined from "@iconify/icons-ant-design/eye-invisible-outlined";
+import settingOutlined from "@iconify/icons-ant-design/setting-outlined";
+import plusCircleOutlined from "@iconify/icons-ant-design/plus-circle-outlined";
+import plusCircleFilled from "@iconify/icons-ant-design/plus-circle-filled";
 
 export default {
-	name: 'TheEventTable',
-	components: {
-		IconifyIcon,
-		TheEventModal,
-	},
-	props: {},
-	data() {
-		return {
-			events: [],
-			isModalVisible: false,
-			selectedEvent: null,
-			icons: {
-				eyeOutlined,
-				eyeInvisibleOutlined,
-				settingOutlined,
-				plusCircleOutlined,
-				plusCircleFilled,
-			},
-		};
-	},
-	computed: {},
-	async created() {
-		bus.$on('modal-close', this.closeModal);
-		let response = await this.$api.event.events();
-		this.events = response;
-	},
+  name: "TheEventTable",
+  components: {
+    IconifyIcon,
+    TheEventModal,
+  },
+  props: {},
+  data() {
+    return {
+      events: [],
+      isModalVisible: false,
+      selectedEvent: null,
+      icons: {
+        eyeOutlined,
+        eyeInvisibleOutlined,
+        settingOutlined,
+        plusCircleOutlined,
+        plusCircleFilled,
+      },
+    };
+  },
+  computed: {},
+  async created() {
+    bus.$on("modal-close", this.closeModal);
+    let response = await this.$api.event.events();
+    this.events = response;
+    bus.$on("event-created", this.addEvent);
+    bus.$on("event-updated", this.updateEvent);
+  },
 
-	destroyed() {
-		bus.$off('modal-close', this.closeModal);
-	},
-	methods: {
-		formattedDate(date) {
-			return moment(date).format('L');
-		},
-		showModal(event) {
-			this.selectedEvent = event ? event : null;
-			this.isModalVisible = true;
-		},
-		closeModal() {
-			this.selectedEvent = null;
-			this.isModalVisible = false;
-		},
-	},
+  destroyed() {
+    bus.$off("modal-close", this.closeModal);
+  },
+  methods: {
+    formattedDate(date) {
+      return moment(date).format("L");
+    },
+    showModal(event) {
+      this.selectedEvent = event ? event : null;
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.selectedEvent = null;
+      this.isModalVisible = false;
+    },
+    addEvent(newEvent) {
+      this.$set(this.events, this.events.length, newEvent);
+    },
+    updateEvent(updatedEvent) {
+      const index = this.events.findIndex(
+        (item) => item.id === updatedEvent.id
+      );
+      this.$set(this.events, index, updatedEvent);
+    },
+  },
 };
 </script>
 
