b085d6f624a2fe670c575e583702cc6f5a467d36
Devon Navon <devon.navon@gmail.com>
2020-08-12 20:29:08 -0700

    len almost got drop down

---
+++ src/components/event/DropDown.vue
@@ -0,0 +1,88 @@
+<!-- src/pages/TheHome/TheCallToAction.vue -->
+<template>
+  <div>
+    <button @click="toggleShow" class="anchor">Plus</button>
+    <div v-if="showMenu" class="menu">
+      <div class="menu-item" v-for="item in this.items" @click="itemClicked(item)">{{item}}</div>
+    </div>
+  </div>
+</template>
+<script>
+import IconifyIcon from "@iconify/vue";
+import closeOutline from "@iconify/icons-carbon/close-outline";
+import bus from "../../bus";
+
+export default {
+  name: "DropDown",
+  data: function () {
+    return {
+      showMenu: false,
+    };
+  },
+  props: function () {
+    return {
+      onClick: "function",
+      items: {
+        type: "Object",
+        default: function () {
+          return [];
+        },
+      },
+    };
+  },
+  methods: {
+    toggleShow: function () {
+      this.showMenu = !this.showMenu;
+    },
+    itemClicked: function (item) {
+      this.toggleShow();
+      this.onClick(item);
+    },
+  },
+};
+</script>
+<style>
+.anchor {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid transparent;
+  padding: 0.75rem 2rem;
+  font-size: 1rem;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  color: #fff;
+  background-color: #27ae60;
+  border-color: #27ae60;
+}
+
+.menu {
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid rgba(0, 0, 0, 0.15);
+  border-radius: 0.25rem;
+  color: #212529;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  font-size: 1rem;
+  list-style: none;
+  margin: 0.125rem 0 0;
+  padding: 0.5rem 0;
+  position: absolute;
+  text-align: left;
+}
+
+.menu-item {
+  color: #212529;
+  padding: 0.25rem 1.5rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.menu-item:hover {
+  background-color: #f4f6f6;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/event/TheEventCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="border border-orange w-full relative pt-8">
+    <button
+      type="button"
+      class="btn-close absolute left-0 top-0 ml-2 mt-2 focus:outline-none transition duration-500 ease-in-out self-center outline-none transform hover:-translate-y-1 hover:scale-105"
+      aria-label="Delete event"
+    >
+      <IconifyIcon
+        :icon="icons.deleteIcon"
+        class="text-orange h-5 w-5 transition duration-500 hover:text-red"
+      />
+    </button>
+    <div class="border border-dotted border-orange w-11/12 relative mx-auto p-5">yo</div>
+    <div class="flex flex-row justify-center py-3">
+      <button class="font-display text-orange text-xl focus:outline-none outline-none self-center">
+        <IconifyIcon
+          :icon="icons.plusCircleOutlined"
+          class="text-orange text-center fill-current transform hover:rotate-180 transition-transform duration-1000 ease-out self-center outline-none focus:outline-none"
+        />
+      </button>
+    </div>
+    <DropDown :on-click="chooseMedia" :items="media"></DropDown>
+  </div>
+</template>
+<script>
+import DropDown from "../event/DropDown.vue";
+
+import IconifyIcon from "@iconify/vue";
+import plusCircleOutlined from "@iconify/icons-ant-design/plus-circle-outlined";
+import plusCircleFilled from "@iconify/icons-ant-design/plus-circle-filled";
+import deleteIcon from "@iconify/icons-wpf/delete";
+import bus from "../../bus";
+
+export default {
+  name: "TheEventCard",
+  components: {
+    DropDown,
+    IconifyIcon,
+  },
+  data() {
+    return {
+      data: {
+        activeMedia: "Text",
+        media: ["Text", "Image"],
+      },
+      icons: {
+        plusCircleOutlined,
+        deleteIcon,
+      },
+    };
+  },
+  methods: {
+    chooseMedia: function (media) {
+      this.activeMedia = media;
+    },
+  },
+};
+</script>
+<style>
+</style>
