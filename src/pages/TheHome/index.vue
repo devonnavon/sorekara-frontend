014c5a93beca1347b7bcf2b9a55ac1c13b5dddf3
Devon Navon <devon.navon@gmail.com>
2020-08-15 00:13:47 -0700

    messed everythnig up

--- src/pages/TheHome/index.vue
+++ src/pages/TheHome/index.vue
@@ -1,23 +1,23 @@
 <!-- home -->
 <template>
-	<div>
-		<TheAppHeader></TheAppHeader>
-		<TheEventTable></TheEventTable>
-	</div>
+  <div>
+    <TheAppHeader></TheAppHeader>
+    <TheEventTable></TheEventTable>
+  </div>
 </template>
 <script>
-import TheEventTable from '../../components/layout/TheEventTable.vue';
-import TheAppHeader from '../../components/layout/TheAppHeader.vue';
+import TheEventTable from "./TheEventTable.vue";
+import TheAppHeader from "./TheAppHeader.vue";
 
 export default {
-	name: 'TheHome',
-	components: {
-		TheAppHeader,
-		TheEventTable,
-	},
-	data() {
-		return {};
-	},
+  name: "TheHome",
+  components: {
+    TheAppHeader,
+    TheEventTable,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 <style></style>
